e55425224549e841a6013c44782e7d67e312c106
marisalen <mlenarduzzi1@my.bcit.ca>
2023-03-31 12:01:25 -0700

    added links

--- comps/heroBanner.js
+++ comps/heroBanner.js
@@ -6,8 +6,8 @@ const HeroBanner = () => {
         <div className={styles.heroText}>
           <h1><em>Hi there,</em> I'm marisa lenarduzzi</h1>
           <p>Designing digital experiences that engage and delight. My passion is creating interfaces that are intuitive, inclusive, and user-friendly. Let's work together to bring your vision to life and make your digital presence accessible to all.</p>
-          <button className={styles.leftButton}>Portfolio</button>
-          <button className={styles.rightButton}>Get in Touch</button>
+          <button className={styles.leftButton}><a href='/about'>Learn More</a></button>
+          <button className={styles.rightButton}><a href='/contact'>Get in Touch</a></button>
         </div>
         <div className={styles.heroImage}>
           <img src="/marisaavatar.png" alt="Hero Image" />
